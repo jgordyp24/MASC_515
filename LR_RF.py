a69ad59ed20128e070499f7c5b6f60e38c6f6c52
jgordyp24 <126610756+jgordyp24@users.noreply.github.com>
2023-03-29 12:11:18 -0700

    Update LR_RF.py

--- LR_RF.py
+++ LR_RF.py
@@ -2,4 +2,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+
+## Reads and loads dataset in pandas DataFrame
+df = pd.read_csv("Dilute_Solute_Diffusion_with_features.csv")
